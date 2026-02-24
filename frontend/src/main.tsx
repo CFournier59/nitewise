d4fe46cdec2f5786616c1cdbb844220ed7765056
CFournier59 <clementfournier.c@gmail.com>
2026-02-24 15:24:28 +0100

    mise en place des routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import "./styles/tailwind.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router'
+import './styles/tailwind.css'
+import './styles/global.css'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-);
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/settings" element={<div>Settings</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/stats" element={<div>Stats</div>} />
+        <Route path="/nites" element={<div>Nites</div>} />
+        <Route path="/nites/create" element={<div>Create Nite</div>} />
+        <Route path="/nites/:niteId" element={<div>Nite Details</div>} />
+        <Route path="/nites/:niteId/edit" element={<div>Edit Nite</div>} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+)
