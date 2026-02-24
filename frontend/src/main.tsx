6d3e0f9c8d7b063a9ab91633b2d95c18f8a02e59
CFournier59 <clementfournier.c@gmail.com>
2026-02-24 16:12:00 +0100

    création des pages Home et Settings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router'
 import './styles/tailwind.css'
 import './styles/global.css'
+import Home from './pages/Home'
+import Settings from './pages/Settings'
 
 async function init() {
   const response = await fetch(`${import.meta.env.BASE_URL}data/nites.json`)
@@ -16,8 +18,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/settings" element={<div>Settings</div>} />
+        <Route path="/" element={<Home nites={nites} />} />
+        <Route path="/settings" element={<Settings />} />
         <Route path="/login" element={<div>Login</div>} />
         <Route path="/register" element={<div>Register</div>} />
         <Route path="/stats" element={<div>Stats</div>} />
