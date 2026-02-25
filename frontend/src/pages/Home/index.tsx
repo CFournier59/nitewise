a2393c23deb1b6d0f7159cabf46494dd1e944b44
CFournier59 <clementfournier.c@gmail.com>
2026-02-25 10:37:53 +0100

    Home : Ajout du titre principal

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ export default function Home({ nites }: { nites: any[] }) {
   return (
     <div className="m-6">
       <Header />
+      <h1 className="text-3xl font-bold mt-6 font-[Libre_Caslon_Text]">
+        Bonjour <span className="text-col2">utilisateur</span>
+      </h1>
     </div>
   )
 }
